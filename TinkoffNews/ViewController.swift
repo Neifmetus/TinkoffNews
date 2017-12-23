489bd257228de23bd842734fed6589934a4528d7
Екатерина Батеева <bateeva-k@mail.ru>
2017-12-23 20:25:49 +0300

    Initial Commit

---
+++ TinkoffNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TinkoffNews
+//
+//  Created by Екатерина Батеева on 23/12/2017.
+//  Copyright © 2017 Екатерина Батеева. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
